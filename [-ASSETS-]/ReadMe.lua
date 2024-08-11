8c79e888b16fa5f90665299c570a5be85e6b5532
BHS-Mayo <bhs.moneymayo@gmail.com>
2024-08-10 23:25:18 -0700

    added files

---
+++ [-ASSETS-]/ReadMe.lua
@@ -0,0 +1,25 @@
+--                            ╔══════════════════════╗
+⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘🄱🄻🄰🄲🄺🄷🄾🄻🄴⫘🅂🅃🅄🄳🄸🄾🅂⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘
+--                            ╚══════════════════════╝
+--
+--
+--    Auther: BHS-Mayo
+--
+--    Heres a simple Resource to keep your FiveM files organized!
+--
+--    How to install:
+--        1- Download the [-ASSETS-] File and place it in your RESOURCES folder
+--
+--        2- In your Server.cfg ensure the following after your CORE RESOURCES
+ensure [-KEYMASTER-]
+ensure [-SCRIPTS-]
+ensure [-STANDALONE-]
+ensure [-WEAPONS-]
+ensure [-CLOTHING-]
+ensure [-MLO-]
+ensure [-VEHICLES-]
+--        3- Download Resources for your FiveM Server and place them in what ever 
+--        folder suits your needs! dont worry about ensuring the scripts as the folder
+--        you placed the script in is already ensured!
+--
+--        4- ENJOY!
